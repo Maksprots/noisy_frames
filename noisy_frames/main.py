8932b1b15bddb4f123139cf413eb924ca2939d45
Maksprots <99216054+Maksprots@users.noreply.github.com>
2022-10-16 23:42:23 +0300

    Update main.py
    trying tooo pep

--- noisy_frames/main.py
+++ noisy_frames/main.py
@@ -5,10 +5,6 @@ import conf
 import frames
 import chooser
 
-import time
-from tqdm import tqdm
-
-
 def deliting(lst_fr, cadr_to_dl):
     new = []
     for i in range(len(lst_fr)):
@@ -22,8 +18,6 @@ def deliting(lst_fr, cadr_to_dl):
 
 
 def save_from_frames(list, fps):
-
-
     file = cv2.VideoWriter(conf.t_conf.out,
                            cv2.VideoWriter_fourcc(*'mp4v'),
                            fps,
@@ -32,21 +26,16 @@ def save_from_frames(list, fps):
         file.write(frame)
     file.release()
 
-
+    
 frames = frames.Frames(conf.t_conf.t_video)
-
-
-chooser = chooser.Chooser(frames.get_brightness_list(), conf.t_conf.deviation)
-
+chooser = chooser.Chooser(frames.get_brightness_list(),
+                          conf.t_conf.deviation)
 if conf.t_conf.METHOD:
     cadr_to_dl = chooser.del_list_brm(
         conf.t_conf.averege_for_frames,
         frames.nuber_frames)
-
     list_fr = frames.get_lst_frames()
-
     new = deliting(list_fr, cadr_to_dl)
-
     save_from_frames(new,
                  frames.fps)
 
