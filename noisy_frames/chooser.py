e98ac0b77de72bd5813b9e399318c271ee15746c
Maksprots <99216054+Maksprots@users.noreply.github.com>
2022-10-16 23:43:27 +0300

    Update chooser.py
    trying toooooo pep

--- noisy_frames/chooser.py
+++ noisy_frames/chooser.py
@@ -3,8 +3,6 @@ import numpy as np
 import statistics
 import conf
 
-
-
 class Chooser:
     def __init__(self, list_bright, deviation):
         self.med_bright = statistics.median(list_bright)
@@ -14,6 +12,7 @@ class Chooser:
 
         self.list_bright = list_bright
 
+        
     def del_list_brm(self, avg_over_frames, nbr_of_fr):
         print('метод: отклонение кадра от средних значений ')
         del_frames = []
@@ -25,22 +24,21 @@ class Chooser:
             for each in range(len(pack)):
                 if max_in_pack - self.list_bright[each + start] > self.abs_deviation:
                     del_frames.append(each + start)
-
         return del_frames
 
+    
     def del_list_dif(self, list_frames):
         print("метод : отклонение разности яркостного канала")
         diferenses = []
         good_frame = cv2.cvtColor(list_frames[0],cv2.COLOR_BGR2GRAY)
         for i in range(1, len(list_frames)):
-
             gray = cv2.cvtColor(list_frames[i], cv2.COLOR_BGR2GRAY)
             diferenses.append(np.sum(cv2.absdiff(good_frame,gray)))
-
         bad = []
         for j in range(0, len(diferenses), conf.t_conf.averege_for_frames):
             if len(diferenses[j:])<10:
                 break
+                
             minf = np.min(diferenses[j:j+10])
             std = np.std(diferenses[j:j+10])
             for i in range(j, j+10):
