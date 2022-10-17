d6abbf37457400976d7c00f5e3db1e27cc90f1ed
Maksprots <99216054+Maksprots@users.noreply.github.com>
2022-10-18 01:20:38 +0300

    Update main.py

--- noisy_frames/main.py
+++ noisy_frames/main.py
@@ -1,9 +1,8 @@
 import cv2
-import numpy as np
-import statistics
 import conf
 import frames
 import chooser
+
 
 def deliting(lst_fr, cadr_to_dl):
     new = []
@@ -17,31 +16,44 @@ def deliting(lst_fr, cadr_to_dl):
     return new
 
 
-def save_from_frames(list, fps):
+def save_from_frames(list, fps, w, h):
+    w = int(w)
+    h = int(h)
+
     file = cv2.VideoWriter(conf.t_conf.out,
                            cv2.VideoWriter_fourcc(*'mp4v'),
                            fps,
-                           (1920, 1080))
-    for frame in list:
-        file.write(frame)
+                           (w, h))
+    for frame in range(len(list)):
+        file.write(list[frame])
     file.release()
 
-    
+
 frames = frames.Frames(conf.t_conf.t_video)
-chooser = chooser.Chooser(frames.get_brightness_list(),
-                          conf.t_conf.deviation)
+
+chooser = chooser.Chooser(
+    frames.get_brightness_list(),
+    conf.t_conf.deviation)
+
 if conf.t_conf.METHOD:
     cadr_to_dl = chooser.del_list_brm(
         conf.t_conf.averege_for_frames,
         frames.nuber_frames)
+
     list_fr = frames.get_lst_frames()
     new = deliting(list_fr, cadr_to_dl)
     save_from_frames(new,
-                 frames.fps)
+                     frames.fps,
+                     frames.width,
+                     frames.height)
 
 else:
     list_fr = frames.get_lst_frames()
-    bad= chooser.del_list_dif(list_fr)
 
-    new =deliting(list_fr, bad)
-    save_from_frames(new, frames.fps)
+    bad = chooser.del_list_dif(list_fr)
+
+    new = deliting(list_fr, bad)
+    save_from_frames(new,
+                     frames.fps,
+                     frames.width,
+                     frames.height)
